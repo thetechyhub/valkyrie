80aaa3dafd8aace74e92195085aaec5cbbb65873
AIMEN .S.A.SASI <aimen.s.a.sasi@AIMENs-MacBook-Air-2.local>
2020-03-04 14:32:50 +0800

    work in progress

--- Modules/Admin/Resources/views/layouts/master.blade.php
+++ Modules/Admin/Resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Module Admin</title>
+    <title>Admin Portal</title>
 
    {{-- Laravel Mix - CSS File --}}
    <link rel="stylesheet" href="{{ mix('css/admin.css') }}">
