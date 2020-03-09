0941e3659ba69beed8b13f93d21fc8dee5033ace
AIMEN .S.A.SASI <aimen.s.a.sasi@AIMENs-MacBook-Air-2.local>
2020-03-09 18:52:38 +0800

    disabled ssl verifing on Http request to passport

--- Modules/Admin/Resources/scripts/store/modules/Session.js
+++ Modules/Admin/Resources/scripts/store/modules/Session.js
@@ -3,7 +3,8 @@ export default {
 	state: {
 		accessToken: null,
 		refreshToken: null,
-		expiresIn: null
+		expiresIn: null,
+		currentUser: {},
 	},
 	getters: { 
 		check(state){
@@ -18,14 +19,17 @@ export default {
 
 			dispatch('validate');
 		},
-		setSession(state, { accessToken, refreshToken, expiresIn }){ 
-			state.accessToken = accessToken;
-			state.refreshToken = refreshToken;
-			state.expiresIn = expiresIn;
+		setSession(state, payload){ 
+			state.accessToken = payload.access_token;
+			state.refreshToken = payload.refresh_token;
+			state.expiresIn = payload.expires_in;
 
-			sessionStorage.setItem("access_token", accessToken);
-			sessionStorage.setItem("refresh_token", refreshToken);
-			sessionStorage.setItem("expire_in", expiresIn);
+			sessionStorage.setItem("access_token", state.accessToken);
+			sessionStorage.setItem("refresh_token", state.refreshToken);
+			sessionStorage.setItem("expire_in", state.expiresIn);
+		},
+		setCurrentUser(state, payload){ 
+			// 
 		},
 		unsetSession: function(){
 			state.accessToken = null;
@@ -40,26 +44,77 @@ export default {
 	actions: {
 		login(context, { email, password }) {
 			let url = route('admin.login').url();
-			axios.post(url, { email, password })
+			return axios.post(url, { email, password })
 			.then(({ data }) => {
-				return Promise.resolve(data);
+				context.commit('setSession', data);
+				return data;
 			}).catch((response) => {
-				return Promise.reject(response);
+				return response;
 			});
 		},
 		logout(context){
 
 		},
-		validate(context){
-			if(context.getters.check) return Promise.resolve(false);
+		refresh(context){
+			let url = route('admin.refresh').url();
+
+			return axios.post(url, { refresh_token: context.state.refreshToken })
+			.then(({ data }) => {
+				context.commit('setSession', data);
+				return true;
+			}).catch((response) => {
+				return false;
+			});
+		},
+		profile({ state, dispatch }){
+			let accessToken = state.refreshToken;
+			let url = route('admin.profile').url();
 
 
+			return axios.get(url, {
+				headers: {
+					'Authorization': `Basic ${accessToken}`
+				}
+			}).then(({ data }) => {
+				return Promise.resolve(true);
+			}).catch(({ response }) => {
+				let status = response.status;
+				if(status == 401){
+					return dispatch('refresh').then((state) => {
+						if(!state){
+							context.commit("unsetSession");
+							return Promise.resolve(false);
+						}
+
+						return Promise.resolve(true);
+					});
+				}
+
+				return Promise.resolve(false);
+			});
+		},
+		validate({ state, dispatch, getters }){
 			// get access token and call api for current user details
 			// if 401 then request new access token
 			// if got 401 again then logout the user and ask him to login again.
 			// add a key to the session marking the last time you validated the user
 			// this will stop the app from making requests to the server everytime since the token can last for more than 1 hour
 			// update this session on each valid request.
+			if(!getters.check) return Promise.resolve(false);
+			let retry = 3;
+			let current = 0;
+			let status = false;
+			while(current <= retry){
+				if (!getters.check) return Promise.resolve(false);
+				dispatch('profile')
+					.then((state) => {
+						status = state;
+						current = retry;		
+					});
+				current++;
+			}
+
+			return Promise.resolve(status);
 		}
 	}
 };
